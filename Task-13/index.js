4305befaa6e6a151d791c6796f905a8ba4ae574c
AbdullahGanatra <Abdullah.ganatra2025@gmail.com>
2026-01-05 08:20:50 +0500

    first commit

---
+++ Task-13/index.js
@@ -0,0 +1,2 @@
+const transportation = ["Royal Infanity", "Royal Royce", "Royal Wonder of the seas", "Lockheed sr-71 Blackbird"];
+transportation.map((transport) => console.log(`I would like to own a ${transport}`));
